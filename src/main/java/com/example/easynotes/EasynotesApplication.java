2eb28dde260de89b1bd66aab6ce947ba1a57d961
vkdn92 <vkdn4292@gmail.com>
2021-02-17 11:15:02 +0530

    first commit

---
+++ src/main/java/com/example/easynotes/EasynotesApplication.java
@@ -0,0 +1,11 @@
+package com.example.easynotes;
+
+
+@SpringBootApplication
+public class EasynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasynotesApplication.class, args);
+	}
+
+}
